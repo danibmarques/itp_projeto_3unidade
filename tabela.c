986d08b784dfd11a55ad52c4c154e4e982ed61a6
marquesdb <44613658+marquesdb@users.noreply.github.com>
2018-11-11 14:57:52 -0200

    Update tabela.c

--- tabela.c
+++ tabela.c
@@ -11,21 +11,27 @@ int main(void) {
   char p = 'n';
   int n_t,i;
   FILE *arquivo = fopen( "sgbd.txt", "w" );
+  if( arquivo == NULL ){
+      printf( "Erro na abertura do arquivo" );
+  }else{
   while(p != 's'){
     printf("Deseja criar uma nova tabela ?(s ou n)");
     scanf("%c", &p);
     printf("Quantas tabelas deseja criar ?");
     scanf(" %i", &n_t);
     Tabela tabelas[n_t];
-   
+
     for(i=0; i<n_t; i++){
-      tabelas[i]=criar_tab; 
+      tabelas[i]=criar_tab;
+     fprintf(arquivo, "%s", tabelas);
     }
   }
   fclose(arquivo);
+  }
 
   return 0;
 }
+
 
 
 
