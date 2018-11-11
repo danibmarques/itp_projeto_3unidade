da7f8b2c6660677ba0e6e35075ac041576b628a7
marquesdb <44613658+marquesdb@users.noreply.github.com>
2018-11-11 01:52:09 -0200

    Update tabela.c

--- tabela.c
+++ tabela.c
@@ -10,16 +10,19 @@ typedef struct tabela{
 int main(void) {
   char p = 'n';
   int n_t,i;
+  FILE *arquivo = fopen( "sgbd.txt", "w" );
   while(p != 's'){
     printf("Deseja criar uma nova tabela ?(s ou n)");
     scanf("%c", &p);
     printf("Quantas tabelas deseja criar ?");
     scanf(" %i", &n_t);
     Tabela tabelas[n_t];
+   
     for(i=0; i<n_t; i++){
-      tabelas[i]=criar_tab;
+      tabelas[i]=criar_tab; 
     }
   }
+  fclose(arquivo);
 
   return 0;
 }
